d7726fb18b6fd7b5f1845adeea9d6bafe046fb8c
John D <redline@Johns-MacBook-Air.local>
2015-12-14 17:36:53 -0600

    Added logic to search for bus closest to user (hardcoded) location

--- client/busTelemetry/busTelemetry.js
+++ client/busTelemetry/busTelemetry.js
@@ -15,16 +15,41 @@ angular.module('wheresMyBusApp.busTelemetry', [])
   };
 
   var findDesiredBusRoute = function(desiredBusRoute, busses){
-
+    var bussesOnDesiredRoute = [];
     for(var busIndex = 0; busIndex < busses.length; busIndex++){
       var currentBus = busses[busIndex];
       var currentRoute = currentBus.Route;
       if(currentRoute === desiredBusRoute){
-        return currentBus;
+        bussesOnDesiredRoute.push(currentBus);
       }
     }
 
-    return null;
+    var currentLatitude = 30.269033; 
+    var currentLongitude = -97.740235;
+    var minimumDistance = Number.MAX_VALUE;
+    var closestBus = null;
+    for(var routeBusIndex = 0; 
+        routeBusIndex < bussesOnDesiredRoute.length;
+        routeBusIndex++)
+    {
+      var routeBus = bussesOnDesiredRoute[routeBusIndex];
+      var routeBusPosition = routeBus.Position;
+      var routeBusLatAndLong = routeBusPosition.split(',');
+      var routeBusLatitude = +routeBusLatAndLong[0];
+      var routeBusLongitude = +routeBusLatAndLong[1];
+
+      var distanceFromRouteBus = getDistanceFromLatLonInKm(
+                                   currentLatitude,
+                                   currentLongitude,
+                                   routeBusLatitude,
+                                   routeBusLongitude);
+      if(distanceFromRouteBus < minimumDistance){
+        minimumDistance = distanceFromRouteBus;
+        closetBus = routeBus;
+      }
+    }
+
+    return closetBus;
   }
 
   var displayData = function(busses){
@@ -52,6 +77,24 @@ angular.module('wheresMyBusApp.busTelemetry', [])
     $scope.speed = currentBus.Speed;
     $scope.position = currentBus.Position;
   }
+
+  var getDistanceFromLatLonInKm = function(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+  };
+
+  var deg2rad = function(deg) {
+    return deg * (Math.PI/180)
+  };
 
   $scope.refreshData();
 });
