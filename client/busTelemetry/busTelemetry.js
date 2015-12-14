2b58e4f80f69c99b180360ff87c1c0872cf58973
John D <redline@Johns-MacBook-Air.local>
2015-12-14 17:17:42 -0600

    Adding marker to map corresponding to bus of interest

--- client/busTelemetry/busTelemetry.js
+++ client/busTelemetry/busTelemetry.js
@@ -1,6 +1,6 @@
 angular.module('wheresMyBusApp.busTelemetry', [])
 
-.controller('telemetryCtrl', function($scope, CapitalMetro){
+.controller('telemetryCtrl', function($scope, $window, CapitalMetro){
   $scope.refreshData = function(){
     CapitalMetro.requestBusData()
     .then(function(data){
@@ -14,13 +14,34 @@ angular.module('wheresMyBusApp.busTelemetry', [])
     });
   };
 
+  var findDesiredBusRoute = function(desiredBusRoute, busses){
+    return busses[0];
+  }
+
   var displayData = function(busses){
-    $scope.updateTime = busses[0].Updatetime;
-    $scope.signage = busses[0].Signage;
-    $scope.route = busses[0].Route;
-    $scope.direction = busses[0].Direction;
-    $scope.speed = busses[0].Speed;
-    $scope.position = busses[0].Position;
+    var currentBus = findDesiredBusRoute('7', busses);
+    var currentBusLatAndLong = currentBus.Position.split(',');
+    var currentBusLatitude = +currentBusLatAndLong[0];
+    var currentBusLongitude = +currentBusLatAndLong[1];
+
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(currentBusLatitude, currentBusLongitude),
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 10
+      },
+      map: $window.mapCanvas,
+      title: 'Current Bus'
+    });
+
+    marker.setMap($window.mapCanvas);
+
+    $scope.updateTime = currentBus.Updatetime;
+    $scope.signage = currentBus.Signage;
+    $scope.route = currentBus.Route;
+    $scope.direction = currentBus.Direction;
+    $scope.speed = currentBus.Speed;
+    $scope.position = currentBus.Position;
   }
 
   $scope.refreshData();
