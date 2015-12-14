a5a1720428b6815ab5d26b57b9d0ec0e19a232c8
John D <redline@Johns-MacBook-Air.local>
2015-12-14 17:04:15 -0600

    Handling server response data and displaying first bus's info

--- client/busTelemetry/busTelemetry.js
+++ client/busTelemetry/busTelemetry.js
@@ -1,18 +1,27 @@
 angular.module('wheresMyBusApp.busTelemetry', [])
 
 .controller('telemetryCtrl', function($scope, CapitalMetro){
-  $scope.updateDisplay = function(){
+  $scope.refreshData = function(){
     CapitalMetro.requestBusData()
     .then(function(data){
       return CapitalMetro.extractBusData(data);
     })
     .then(function(busses){
-      var x = busses;
+      displayData(busses);
     })
     .catch(function (error){
       console.log(error);
     });
   };
 
-  $scope.updateDisplay();
+  var displayData = function(busses){
+    $scope.updateTime = busses[0].Updatetime;
+    $scope.signage = busses[0].Signage;
+    $scope.route = busses[0].Route;
+    $scope.direction = busses[0].Direction;
+    $scope.speed = busses[0].Speed;
+    $scope.position = busses[0].Position;
+  }
+
+  $scope.refreshData();
 });
